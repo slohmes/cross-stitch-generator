910a8436e93ab5b223e727e6589b44151ffb497b
Sarah Lohmeier <sarahlohmes@gmail.com>
2018-04-30 14:17:03 -0400

    displays input text box

--- test.js
+++ test.js
@@ -3,23 +3,19 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const dir = './tmp';
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    userDataDir: `${dir}/mockUserDataDir`//'/Users/sarahlohmeier/projects/js-sample-app/tmp/mockUserDataDir'
+    userDataDir: `${dir}/mockUserDataDir`
   });
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
-  await page.waitForFunction('document.querySelector("p").innerHTML.length > 0').catch(err => console.log("something's wrong", err));
+  await page.waitForFunction('document.querySelector("p").innerHTML.length > 0').catch(err => console.log('something\'s wrong', err));
   await page.screenshot({path: `${dir}/localhost3000-4.png`});
 
   console.log('saved screenshot');
   await browser.close();
   process.exit(0);
 })();
-
-// add in an else here - if something goes wrong, exit.
