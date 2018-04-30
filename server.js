205d6b6ff1fb67c5032c898d8f74b3906df9e935
Sarah Lohmeier <sarahlohmes@gmail.com>
2018-04-30 16:35:24 -0400

    Display entered text beneath input box

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public/images'));
+
+app.get('/', function(request, response) {
+	response.type('text/html');
+	response.sendFile(path.join(__dirname+'/public/home.html'));
+});
+
+// custom 404 page
+app.use(function(request, response) {
+	response.type('text/html');
+	response.status(404);
+	response.sendFile(path.join(__dirname+'/public/404.html'));
+});
+
+// custom 500 page
+app.use(function(error, request, response, next) {
+	console.error(error.stack);
+	response.type('text/plain');
+	response.status(500);
+	response.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+	console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl + c to terminate.');
+
+});
